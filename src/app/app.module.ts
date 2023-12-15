d7e1cf294fbfde1ef02e0e34aff988d4ee6def3e
mahrez <mahreztouzi@gmail.com>
2023-12-15 21:13:36 +0100

    creeation de la modale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,22 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
+import { SimulationComponent } from './simulation/simulation.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SimulationComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: '', component: LoginComponent },
-      { path: 'home', component: HomeComponent },
+      { path: 'signIn', component: LoginComponent },
+      { path: '', component: HomeComponent },
+      { path: 'simulation', component: SimulationComponent },
     ]),
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  public showModale: boolean = false;
+  public toggleModale(): any {
+    this.showModale = !this.showModale;
+  }
 }
